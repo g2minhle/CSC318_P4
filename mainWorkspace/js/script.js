6e8fcb73b23e81e5db752fbe4993ab443343b3b6
root <root@localhost.localdomain>
2015-11-27 13:46:56 -0500

    Add initial layout for main workspace

---
+++ mainWorkspace/js/script.js
@@ -0,0 +1,26 @@
+$("#submit").on("click", function(e){
+  e.preventDefault();
+  var newPost = $("#post-2").clone();
+  
+  var date = {
+    time: new Date().getHours() + ":" +
+         ((new Date().getMinutes() > 10) ? new Date().getMinutes()
+                                       : "0" + new Date().getMinutes()),
+    date: new Date().getMonth() + "/" +
+          new Date().getDay() + "/" +
+          new Date().getFullYear()
+  }
+
+  newPost.find(".panel-body").html(
+    $("#input-content").val() + 
+    '<div class="post-footer">Posted at ' + date.time + ' on ' + date.date + '.</div>'                  
+  );
+
+  newPost.find("img").attr("src", "images/myAvatar.png");
+  newPost.find("figcaption").text("Elliot Smith (You)");
+
+
+  newPost.appendTo("#post-list");
+  $("#input-content").val("");
+
+});
