4ee93021b0333b07869bdcfcdda1e529b713338b
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2015-12-05 18:40:06 -0500

    now it's possible to add more tracks from a file

--- mainWorkspace/js/script.js
+++ mainWorkspace/js/script.js
@@ -1,83 +1,90 @@
 var barStorage = {};
 var currentId = null;
 
-function a() {	
-	if(window.location.search == "?new=1"){
-		$('.track').hide();	
-	}
-	$('#div_Lyric').draggable({ containment: 'parent', axis: 'x' });
-	$('.js-track-images .trackLength').draggable({ 
-		containment: 'parent', 
-		axis: 'x' ,
-		start : function(){
-			console.log('start');
-			var trackID = $(this).id;
-			var offset = $(this).offset();
-			$('.' + $(this).attr('id') ).each(function (index ){
-				console.log(offset);
-				$(this).offset({ 
-					top: offset.top + $(this).data('top-offset'), 
-					left: offset.left + $(this).data('left-offset')
-				});			
-			});	
-		},
-		drag : function(){
-			var trackID = $(this).id;
-			console.log('draging');			
-			var offset = $(this).offset();
-			$('.' + $(this).attr('id') ).each(function (index ){
-				console.log(offset);
-				$(this).offset({ 
-					top: offset.top + $(this).data('top-offset'), 
-					left: offset.left + $(this).data('left-offset')
-				});			
-			});				
-		}});
 
-	$('.js-track-images .trackLength').contextmenu(function(e) {
+draggableObject = {
+	containment: 'parent',
+	axis: 'x' ,
+	start : function(){
+		console.log('start');
+		var trackID = $(this).id;
+		var offset = $(this).offset();
+		$('.' + $(this).attr('id') ).each(function (index ){
+			console.log(offset);
+			$(this).offset({
+				top: offset.top + $(this).data('top-offset'),
+				left: offset.left + $(this).data('left-offset')
+			});
+		});
+	},
+	drag : function(){
+		var trackID = $(this).id;
+		console.log('draging');
+		var offset = $(this).offset();
+		$('.' + $(this).attr('id') ).each(function (index ){
+			console.log(offset);
+			$(this).offset({
+				top: offset.top + $(this).data('top-offset'),
+				left: offset.left + $(this).data('left-offset')
+			});
+		});
+	}};
+
+
+	function addComment(e) {
 		var comment = prompt("Please enter your comment", "My comment");
-  		if (comment == null) return;
-		var id = Math.random(); 
+			if (comment == null) return;
+		var id = Math.random();
 		var trackID = $(this).attr('id');
-		var parentOffset = $(this).offset();    
+		var parentOffset = $(this).offset();
 		var relX = e.pageX - parentOffset.left;
 		var relY = e.pageY - parentOffset.top;
 
 		var str = $('<button id = "btn_'+ id+'" type="button"' +
 						'class="btn btn-xs btn-warning comment ' + trackID + '"' +
 						'data-toggle="popover"'+
-						'title="Me"' + 
+						'title="Me"' +
 						'data-trigger="focus"' +
 						'data-content="' + comment +'"' +
 						'data-top-offset="' + relY +'"' +
 						'data-left-offset="' + relX +'">' +
 						'<span class="glyphicon glyphicon-comment" aria-hidden="true"></span>' +
 												'</button>');
-		str.offset({ 
+		str.offset({
 			top: parentOffset.top + relY,
 			left: relX
-		});	
+		});
 		str.popover();
-		$(this).parent().append(str);		
-				
+		$(this).parent().append(str);
+
 		$('.comment').contextmenu(function(){
 			$(this).hide();
-			return false;	
-		});		
-		
+			return false;
+		});
+
 		var trackID = $(this).id;
-			console.log('draging');			
+			console.log('draging');
 			var offset = $(this).offset();
 			$('.' + $(this).attr('id') ).each(function (index ){
 				console.log(offset);
-				$(this).offset({ 
-					top: offset.top + $(this).data('top-offset'), 
+				$(this).offset({
+					top: offset.top + $(this).data('top-offset'),
 					left: offset.left + $(this).data('left-offset')
-				});			
+				});
 			});
-		
+
 		return false;
-	});
+	}
+
+function a() {
+	if(window.location.search == "?new=1"){
+		$('.track').hide();
+	}
+	$('#div_Lyric').draggable({ containment: 'parent', axis: 'x' });
+	$('.js-track-images .trackLength').draggable(draggableObject);
+
+
+	$('.js-track-images .trackLength').contextmenu(addComment);
 	// $('#volumeSlider').slider();
 	// $('#bassSlider').slider();
 	// $('#middleSlider').slider();
@@ -91,8 +98,8 @@ function a() {
 
 $('.comment').contextmenu(function(){
 	$(this).hide();
-	return false;	
-});		
+	return false;
+});
 
 function cmd_save(){
 	$.notify('Change saved', 'success');
@@ -113,11 +120,11 @@ $('.js-close-track').on('click', function(e){
 $('.track').on('click', function(e){
 	var itemID = $(this).attr('id');
 	if(itemID == 'div_lyric') return;
-	
+
 	$('.track').removeClass('selected');
 	$(this).addClass('selected');
 
-	// save current 
+	// save current
 	if(currentId != null){
 		barStorage[currentId] = {};
 		barStorage[currentId]['volumeSlider'] = $('#volumeSlider').val();
@@ -125,20 +132,20 @@ $('.track').on('click', function(e){
 		barStorage[currentId]['bassSlider'] = $('#bassSlider').val();
 		barStorage[currentId]['middleSlider'] = $('#middleSlider').val();
 		barStorage[currentId]['trebbleSlider'] = $('#trebbleSlider').val();
-		console.log(barStorage);	
+		console.log(barStorage);
 	}
-	
+
 	// change slider
 	var trackId = itemID.replace("div_track", "");
-	
+
 	currentId = trackId;
-	
+
 	if(trackId in barStorage){
 		$('#volumeSlider').val(barStorage[trackId]['volumeSlider']);
-		$('#panSlider').val(barStorage[trackId]['panSlider']);		
+		$('#panSlider').val(barStorage[trackId]['panSlider']);
 		$('#bassSlider').val(barStorage[trackId]['bassSlider']);
 		$('#middleSlider').val(barStorage[trackId]['middleSlider']);
-		$('#trebbleSlider').val(barStorage[trackId]['trebbleSlider']);		
+		$('#trebbleSlider').val(barStorage[trackId]['trebbleSlider']);
 	} else {
 		$('#volumeSlider').val(50);
 		$('#panSlider').val(50);
@@ -148,22 +155,23 @@ $('.track').on('click', function(e){
 	}
 });
 
-$('.js-display-wave').on('click', function(){
+
+$('body').on('click', '.js-display-wave', function(){
 	var trackColumn = $(this).closest('.track');
 	trackColumn.find('.trackLength').attr('src',
 		trackColumn.find('.trackLength').data('srcwaves')
 	);
 });
-$('.js-display-sheet').on('click', function(){
+$('body').on('click', '.js-display-sheet', function(){
 	var trackColumn = $(this).closest('.track');
 
 	trackColumn.find('.trackLength').attr('src',
 		trackColumn.find('.trackLength').data('srcsheets')
 	);
 });
-$('.js-display-tab').on('click', function(){
+$('body').on('click', '.js-display-tab', function(){
 	var trackColumn = $(this).closest('.track');
-	
+
 	trackColumn.find('.trackLength').attr('src',
 		trackColumn.find('.trackLength').data('srctabs')
 	);
@@ -175,17 +183,28 @@ function cmd_addTracK(){
 
 function input_fileUploaded(){
 	$.notify('Track added', 'success');
-	tracks = $(".track:hidden").first().show();
+
+	//CLONE track
+	var id = parseInt(Math.random() * 1000);
+	var newTrack = $(".track:first").clone();
+	newTrack.attr("id", "div_track" + id);
+	newTrack.find("h3").text("New Track");
+	newTrack.find(".comment").remove();
+	newTrack.find("img").attr("id", id);
+	newTrack.find(".trackLength").draggable(draggableObject);
+	newTrack.find(".trackLength").contextmenu(addComment);
+	$(".trackList").append(newTrack);
+
 }
 
 function cmd_play(){
 	var audio = new Audio('./fullSong.mp3');
-	audio.play();	
+	audio.play();
 }
 
 function cmd_addLyric(){
 	$('#div_lyric').show();
-	$('#input_Lyric').focus();	
+	$('#input_Lyric').focus();
 }
 
 function alertMsg() {
